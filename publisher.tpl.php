10c158da5f5e0d83d26650155381387c05bda671
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-03-16 07:04:46 +0500

    stupid ng-hide

--- publisher.tpl.php
+++ publisher.tpl.php
@@ -116,14 +116,10 @@
                         </div>
 
                         <div ng-repeat="(k, v) in errors" ng-show="errors">
-                            <div class="alert alert-danger alert-dismissible" role="alert" ng-show="submitted">
+                            <div class="alert alert-danger alert-dismissible" role="alert">
                                 <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                                 <strong>{{ key }}: </strong> {{ value }}
                             </div>
-                        </div>
-
-                        <div>
-                            <pre>{{ errors }}</pre>
                         </div>
                     
                         <md-content class="md-padding" ng-hide="submitted">
