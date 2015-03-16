c4e723ddd4625aa4addcfc9e0c4e678101b32511
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-03-16 05:49:22 +0500

    js | reindentation

--- js/registration.js
+++ js/registration.js
@@ -1,45 +1,41 @@
-var app = angular.module('StarterApp', ['ngMaterial']);
-
-    app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-        $scope.toggleSidenav = function(menuId) {
-            $mdSidenav(menuId).toggle();
-        },
-        $scope.user = {
-
-        };
-    }]).config( function($mdThemingProvider){
+var app = angular.module('StarterApp', ['ngMaterial'])
+app.config( function($mdThemingProvider){
         // Configure a dark theme with primary foreground yellow
         $mdThemingProvider.theme('docs-dark', 'default')
     });
 
-    app.directive("passwordVerify", function() {
-        return {
-            require: "ngModel",
-            scope: {
-                passwordVerify: '='
-            },
-            link: function(scope, element, attrs, AppCtrl) {
-                scope.$watch(function() {
-                    var combined;
+app.controller('AppCtrl', function($scope) {
+    console.log('I am the king')
+});
 
-                    if (scope.passwordVerify || AppCtrl.$viewValue) {
-                        combined = scope.passwordVerify + '_' + AppCtrl.$viewValue; 
-                    }                    
-                    return combined;
-                }, function(value) {
-                    if (value) {
-                        AppCtrl.$parsers.unshift(function(viewValue) {
-                            var origin = scope.passwordVerify;
-                            if (origin !== viewValue) {
-                                AppCtrl.$setValidity("passwordVerify", false);
-                                return undefined;
-                            } else {
-                                AppCtrl.$setValidity("passwordVerify", true);
-                                return viewValue;
-                            }
-                        });
-                    }
-                });
-            }
-        };
-    });
+app.directive("passwordVerify", function() {
+    return {
+        require: "ngModel",
+        scope: {
+            passwordVerify: '='
+        },
+        link: function(scope, element, attrs, AppCtrl) {
+            scope.$watch(function() {
+                var combined;
+
+                if (scope.passwordVerify || AppCtrl.$viewValue) {
+                    combined = scope.passwordVerify + '_' + AppCtrl.$viewValue; 
+                }                    
+                return combined;
+            }, function(value) {
+                if (value) {
+                    AppCtrl.$parsers.unshift(function(viewValue) {
+                        var origin = scope.passwordVerify;
+                        if (origin !== viewValue) {
+                            AppCtrl.$setValidity("passwordVerify", false);
+                            return undefined;
+                        } else {
+                            AppCtrl.$setValidity("passwordVerify", true);
+                            return viewValue;
+                        }
+                    });
+                }
+            });
+        }
+    };
+});
