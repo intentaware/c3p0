e1369d996fae183bf2727b9d33765da71b362a45
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-03-16 05:39:23 +0500

    script moved into a seperate file for DRY

--- adevrtiser.tpl.php
+++ adevrtiser.tpl.php
@@ -105,7 +105,7 @@
 
         <div class="row">
             <div class="container">
-            <form name="registrationForm">
+
                 <div layout="column" flex id="content">
                     <md-content layout="column" flex class="md-padding">
                         <?php 
@@ -174,7 +174,6 @@
 
                     </md-content>
                 </div>
-            </form>
 
             </div>
         </div>
@@ -189,52 +188,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.6/angular-aria.min.js"></script>
 
 <script src="https://ajax.googleapis.com/ajax/libs/angular_material/0.7.1/angular-material.min.js"></script>
-<script type="text/javascript">
-    var app = angular.module('StarterApp', ['ngMaterial']);
 
-    app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-        $scope.toggleSidenav = function(menuId) {
-            $mdSidenav(menuId).toggle();
-        },
-        $scope.user = {
-
-        };
-    }]).config( function($mdThemingProvider){
-        // Configure a dark theme with primary foreground yellow
-        $mdThemingProvider.theme('docs-dark', 'default')
-    });
-
-    app.directive("passwordVerify", function() {
-        return {
-            require: "ngModel",
-            scope: {
-                passwordVerify: '='
-            },
-            link: function(scope, element, attrs, AppCtrl) {
-                scope.$watch(function() {
-                    var combined;
-
-                    if (scope.passwordVerify || AppCtrl.$viewValue) {
-                        combined = scope.passwordVerify + '_' + AppCtrl.$viewValue; 
-                    }                    
-                    return combined;
-                }, function(value) {
-                    if (value) {
-                        AppCtrl.$parsers.unshift(function(viewValue) {
-                            var origin = scope.passwordVerify;
-                            if (origin !== viewValue) {
-                                AppCtrl.$setValidity("passwordVerify", false);
-                                return undefined;
-                            } else {
-                                AppCtrl.$setValidity("passwordVerify", true);
-                                return viewValue;
-                            }
-                        });
-                    }
-                });
-            }
-        };
-    });
-
-</script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/registration.js" type="text/javascript"></script>
 <?php get_footer(); ?>
