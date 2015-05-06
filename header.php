fe20b8ec53fc707aab8615c407c1ce2c9c0fc8df
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-05-06 09:23:50 +0500

    fixing login path

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
 
     <script>(function(){document.documentElement.className='js'})();</script>
 
-    <?php 
+    <?php
         if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
             // enqueue the javascript that performs in-link comment reply fanciness
             wp_enqueue_script( 'comment-reply' );
@@ -40,14 +40,14 @@
     ?>
 
     <?php wp_head(); ?>
-    
+
     <style type="text/css">
         * {
             outline: none;
         }
-        
+
         html{margin-top: 0 !important;}
-        
+
         #scrollerota {
             width: 500px;
             height: 333px;
@@ -132,25 +132,25 @@
         #scrollerota .controls .play {
             background-position: -23px 0;
             display: none;
-        }    
+        }
 
         ul#demo-block{ margin:0 15px 15px 15px; }
         ul#demo-block li{ margin:0 0 10px 0; padding:10px; display:inline; float:left; clear:both; color:#aaa; background:url('<?php echo get_template_directory_uri(); ?>/img/bg-black.png'); font:11px Helvetica, Arial, sans-serif; }
         ul#demo-block li a{ color:#eee; font-weight:bold; }
     </style>
-    
+
     <script type="text/javascript">
         jQuery(document).ready(function(){
             var menu = jQuery('#bs-example-navbar-collapse-1').html();
             jQuery('#navbar').html(menu);
-            
+
             jQuery('#btnNavBar').click(function(){
                 jQuery('#blankHeight').slideToggle('slow');
             });
-            
+
         });
     </script>
-    
+
 </head>
 
 <body <?php body_class(); ?> layout="column" ng-controller="AppCtrl">
@@ -160,9 +160,9 @@
         <div class="container">
             <div class="navbar-header" style="text-align: right;">
                 <?php if(!is_front_page()){ ?>
-                    <a href="#" class="btn btn-big btn-black btn-login" style="/*float:right; */margin:5px 10px;">Login</a>
+                    <a href="http://app.adomattic.com/" class="btn btn-big btn-black btn-login" style="/*float:right; */margin:5px 10px;">Login</a>
                 <?php } ?>
-                
+
                 <a class="navbar-brand" href="<?php echo get_bloginfo('siteurl'); ?>"><img src="<?php echo stripslashes(urldecode(get_option('site_logo'))); ?>" style="margin-top:-10px;"></a>
                 <button type="button" class="navbar-toggle collapsed" id="btnNavBar" data-toggle="collapse" data-target="#navbar">
                     <span class="sr-only">Toggle navigation</span>
