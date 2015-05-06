6fd9895b224fa521a2179c135823b68326db24ba
Yousuf Jawwad <yousuf.jawwad@gmail.com>
2015-05-06 09:17:04 +0500

    adding initial value directive

--- js/registration.js
+++ js/registration.js
@@ -67,6 +67,32 @@ app.directive("passwordVerify", function() {
     };
 });
 
-jQuery(function($){   
+app.directive('initialValue', function() {
+  return{
+    restrict: 'A',
+    controller: ['$scope', '$element', '$attrs', '$parse', function($scope, $element, $attrs, $parse){
+
+      var getter, setter, val, tag;
+      tag = $element[0].tagName.toLowerCase();
+
+      val = $attrs.initialValue || $element.val();
+      if(tag === 'input'){
+        if($element.attr('type') === 'checkbox'){
+          val = $element[0].checked ? true : undefined;
+        } else if($element.attr('type') === 'radio'){
+          val = ($element[0].checked || $element.attr('selected') !== undefined) ? $element.val() : undefined;
+        }
+      }
+
+      if($attrs.ngModel){
+        getter = $parse($attrs.ngModel);
+        setter = getter.assign;
+        setter($scope, val);
+      }
+    }]
+  };
+});
+
+jQuery(function($){
    $("#phone").mask("999 - 999 - 9999");
 });
