3c91900bab3b5f45e9b462307c39f8464bc20b7e
Rehman Shafi <manichilll@yahoo.com>
2015-11-10 20:29:22 +0500

    Content Division
    Content re aligned.
    Font Styling.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<link href="wp-includes/css/bootstrap.min.css" rel="stylesheet">
 		<link href="wp-includes/css/style.css" rel="stylesheet">
 	</head>
-	<body>
+	<body style="font-family: Fira Sans Light">
 		<nav class="navbar navbar-default " style=" margin-top:-50px ; z-index:-1; background-color:white;" >
         </nav>
 		<div class="row">
@@ -12,50 +12,42 @@
 				<li style="margin-top:-50px;  text-align:center; list-style: none;"><a href="#" id="spin"><img src="wp-includes/images/logo.png" id="logo"></a></li>
 			</div>
 		</div>
-        
-            <div id="container">     
-				<div class="row" style="margin-top: 330px; margin-bottom: 20px">
-					<div class="col-lg-4" >
-						<button class="pull-right btn btn-primary" style="background-color: #CD162D; border-color: #CD162D"> Advertiser </button>
-					</div>
-					<div class="col-lg-4">
-      
-					</div>
-					<div class="col-lg-4">
-						<button class="btn btn-primary" style="background-color: #CD162D; border-color: #CD162D"> Publisher </button>
-					</div>
-				</div>
-            </div>
-            <div class="container">
-				<div class="panel panel-default" style="background-color: rgba(245, 245, 245, 0.4);">
-					<div class="panel-body" style="font-family: Fira Sans;">
-						<h3>The World's First Intent Aware Advertising</h3>
-						<p>
+		<div class="container">
+			<div class="row" style="margin-top: 145px; margin-bottom: 150px">
+				<div class="col-lg-8">
+					<h3 style="font-family: Fira Sans Bold; font-size:270%">The World's First Intent Aware Advertising</h3>
+						<p style="font-size:120%">
 							We are IntentAware, and we have fundamentally changed how display ads function on internet.
 							Through data-aware ads we enable advertisers and publishers to get unbelievable conversion rates.
 							<br>
 							IntentAware makes AD's Intelligent by leveraging Artificial Intelligence and Machine Learning.
 							This makes our engine better than retargeting, contextual ads or any other ads.
 						</p>
-					</div>
+				</div>
+				<div class="col-lg-4" style="margin-top: 25px">
+				
+					<button class="btn btn-primary" style="background-color: #CD162D; border-color: #CD162D; width:115px"> Advertiser </button>
+					<div class="row" style="padding:20px"></div>
+					<button class="btn btn-primary" style="background-color: #CD162D; border-color: #CD162D; width:115px"> Publisher </button>
 				</div>
 			</div>
-			<div class= "nav navbar-default ">
-				<div class="container-fluid">
-					<ul class="nav navbar-nav navbar-center" style=" margin-left:350px ;" >
-						<li><a href="#">Platforms</a></li>
-						<li><a href="#">Labs</a></li>
-						<li><a href="#">Articles</a></li>
-						<li><a href="#">Career</a></li>
-						<li><a href="#">Contact</a></li>
-						<li><a href="#">Privacy</a></li>
-						<li><a href="#">Opt-Out</a></li>
-						<li><a href="#">Brand Guidelines</a></li>
-					</ul>
-				</div>
+		</div>
+		<div class= "nav navbar-default ">
+			<div class="container-fluid">
+				<ul class="nav navbar-nav navbar-center" style=" margin-left:350px ;" >
+					<li><a href="#">Platforms</a></li>
+					<li><a href="#">Labs</a></li>
+					<li><a href="#">Articles</a></li>
+					<li><a href="#">Career</a></li>
+					<li><a href="#">Contact</a></li>
+					<li><a href="#">Privacy</a></li>
+					<li><a href="#">Opt-Out</a></li>
+					<li><a href="#">Brand Guidelines</a></li>
+				</ul>
 			</div>
+		</div>
 			
-		<script src="js/jquery-1.11.3.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
+		<script src="wp-includes/js/jquery-1.11.3.min.js"></script>
+		<script src="wp-includes/js/bootstrap.min.js"></script>
 	</body>
 </html>
