c98bd38a4d3dbaa645000e7d05d818ee1a57df8a
Rehman <manichilll@yahoo.com>
2015-11-29 23:27:33 +0500

    about page skeleton up

--- site/web/app/themes/intentaware/templates/about.php
+++ site/web/app/themes/intentaware/templates/about.php
@@ -1,4 +1,18 @@
 <!--Template Name: about -->
-<?php $page = (get_page_by_title("About")); 
-    echo $page->post_content;
-?>
+<?php $page = (get_page_by_title("About")); ?>
+<div id="tf-about">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="img/02.png" class="img-responsive">
+                </div>
+                <div class="col-md-6">
+                    <div class="about-text">
+                        <div class="section-title">
+                            <h4>About us</h4>
+                            <?php echo $page->post_content; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
