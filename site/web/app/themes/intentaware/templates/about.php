e29de804cfeec107878dcf951226ea1ac819fdb0
Rehman <manichilll@yahoo.com>
2015-11-30 00:22:13 +0500

    Fully functional about page

--- site/web/app/themes/intentaware/templates/about.php
+++ site/web/app/themes/intentaware/templates/about.php
@@ -4,13 +4,17 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <img src="img/02.png" class="img-responsive">
+                    <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/02.png" class="img-responsive">
                 </div>
                 <div class="col-md-6">
                     <div class="about-text">
                         <div class="section-title">
                             <h4>About us</h4>
-                            <?php echo $page->post_content; ?>
+                            <h2>Some words <strong>about us</strong></h2>
+                            <hr>
+                            <div class="clearfix"></div>
+                        </div>	
+                        <p class="intro"><?php echo $page->post_content; ?></p>
                     </div>
                 </div>
             </div>
