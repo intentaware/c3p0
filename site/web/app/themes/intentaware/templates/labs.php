f6a3f4d9b8bf2773a690bb52b4de396341a2c9a3
Rehman <manichilll@yahoo.com>
2015-11-30 19:08:54 +0500

    Labs page included

---
+++ site/web/app/themes/intentaware/templates/labs.php
@@ -0,0 +1,17 @@
+<!--Template Name: labs -->
+<?php $page = (get_page_by_title("Labs")); ?>
+<div id="tf-clients" class="text-center">
+        <div class="overlay">
+            <div class="container">
+
+                <div class="section-title center">
+                    <h2>Some of <strong><?php echo $page->post_title; ?></strong></h2>
+                    <div class="line">
+                        <hr>
+                    </div>
+                    <div class="clearfix"></div>
+                    <p class="intro"><?php echo $page->post_content; ?></p>
+                </div>
+            </div>
+        </div>
+    </div>
