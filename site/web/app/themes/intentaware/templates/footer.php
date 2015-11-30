07973fee35f83d79975c19915157297cc189fb76
Rehman <manichilll@yahoo.com>
2015-11-30 20:11:57 +0500

    Footer template

--- site/web/app/themes/intentaware/templates/footer.php
+++ site/web/app/themes/intentaware/templates/footer.php
@@ -1,7 +1,10 @@
 </body>
-<footer class="content-info">
-  <div class="container">
-  This is Footer.
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
-</footer>
+<div class="footer">
+	<nav id="footer">
+	    <div class="container">
+	        <div class="pull-left fnav">
+	            <p>ALL RIGHTS RESERVED. COPYRIGHT © 2014. Designed by <a href="https://dribbble.com/shots/1817781--FREEBIE-Spirit8-Digital-agency-one-page-template">Robert Berki</a> and Coded by <a href="https://dribbble.com/jennpereira">Jenn Pereira</a></p>
+	        </div>
+	    </div>
+	</nav>
+</div>
