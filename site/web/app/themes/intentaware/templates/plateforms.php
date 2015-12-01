26e7855a2b2f90d2bf3f1e85b7d779d22f72fc4d
Rehman <manichilll@yahoo.com>
2015-12-01 18:10:53 +0500

    Featured images support

--- site/web/app/themes/intentaware/templates/plateforms.php
+++ site/web/app/themes/intentaware/templates/plateforms.php
@@ -1,6 +1,18 @@
  <!--Template Name: plateforms -->
 
-<?php $page = (get_page_by_title("Our Platforms")); ?>
+<?php $page = (get_page_by_title("Our Platforms"));
+    if(has_post_thumbnail($page->ID))
+    {
+        $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) ); 
+        print_r($image);
+        ?>
+        
+        <div id="tf-services" class="text-center" style="background-image: url('<?php echo $image[0]; ?>')">
+
+        <?php
+
+    }
+ ?>
  <div id="tf-services" class="text-center">
         <div class="container">
             <div class="section-title center">
@@ -14,3 +26,9 @@
             <div class="space"></div>
         </div>
     </div>
+    <?php 
+        $check =  has_post_thumbnail($page->ID);
+        $packet = get_the_post_thumbnail($page->ID);
+        $src = preg_match('@^(src=)?([^"]+)@i', $packet, $matches);
+        var_dump(wp_get_attachment_image_src( get_post_thumbnail_id($post->ID) ));
+     ?>
