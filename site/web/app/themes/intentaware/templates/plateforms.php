441312f69d7d850136716005667c6beebb6b0929
Rehman <manichilll@yahoo.com>
2015-12-01 18:51:04 +0500

    Dynamic feature image support added to platforms page

--- site/web/app/themes/intentaware/templates/plateforms.php
+++ site/web/app/themes/intentaware/templates/plateforms.php
@@ -1,19 +1,35 @@
  <!--Template Name: plateforms -->
-
 <?php $page = (get_page_by_title("Our Platforms"));
+        $packet = get_the_post_thumbnail($page->ID);
+        $src = preg_match_all('@[(a-zA-z//:0-9/.)]+@i', $packet, $matches);
+        $image = ($matches[0][6]);
+
     if(has_post_thumbnail($page->ID))
     {
-        $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) ); 
-        print_r($image);
         ?>
-        
-        <div id="tf-services" class="text-center" style="background-image: url('<?php echo $image[0]; ?>')">
+        <div id="tf-services" class="text-center imaged" style="background-image: url('<?php echo $image; ?>')">
+            <div class="overlay">
+                <div class="container">
+                    <div class="section-title center">
+                    <h2>Take a look at <strong><?php echo $page->post_title; ?></strong></h2>
+                    <div class="line">
+                    <hr>
+                    </div>
+                    <div class="clearfix"></div>
+                <p class="intro"><?php echo $page->post_content; ?></p>
+            </div>
+            <div class="space"></div>
+                </div>
+            </div>
+        </div>  
 
         <?php
 
     }
- ?>
- <div id="tf-services" class="text-center">
+    else
+    {
+        ?>
+        <div id="tf-services" class="text-center">
         <div class="container">
             <div class="section-title center">
                 <h2>Take a look at <strong><?php echo $page->post_title; ?></strong></h2>
@@ -26,9 +42,6 @@
             <div class="space"></div>
         </div>
     </div>
-    <?php 
-        $check =  has_post_thumbnail($page->ID);
-        $packet = get_the_post_thumbnail($page->ID);
-        $src = preg_match('@^(src=)?([^"]+)@i', $packet, $matches);
-        var_dump(wp_get_attachment_image_src( get_post_thumbnail_id($post->ID) ));
-     ?>
+    <?php
+    }
+ ?>
