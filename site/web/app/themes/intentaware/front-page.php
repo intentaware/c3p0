e01de558ea4f287a0efce2c1a1f37a831ebc8f54
Rehman <manichilll@yahoo.com>
2015-12-02 13:46:26 +0500

    Brandguideline template section included.

--- site/web/app/themes/intentaware/front-page.php
+++ site/web/app/themes/intentaware/front-page.php
@@ -16,4 +16,5 @@
 <?php get_template_part('templates/about'); ?>
 <?php get_template_part('templates/plateforms'); ?>
 <?php get_template_part('templates/labs'); ?>
+<?php get_template_part('templates/brandGuideline'); ?>
 <?php get_template_part('templates/contact'); ?>
