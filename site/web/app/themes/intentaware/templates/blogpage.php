a5ef8ee19fb4c96a14cd7c0963fde3c2ccb4d9ff
Rehman <manichilll@yahoo.com>
2015-12-09 02:42:38 +0500

    Blog page modifications.

--- site/web/app/themes/intentaware/templates/blogpage.php
+++ site/web/app/themes/intentaware/templates/blogpage.php
@@ -5,32 +5,55 @@
 ?>
 <div class="container">
 <button class="btn tf-btn btn-default"><a href="<?php echo(home_url()) ?>"><span class="glyphicon glyphicon-menu-left"></span> Home </a></button>
-<div class="text-center">
-<div class="section-title center" style="min-height: 650px" >
-                <h2>Read Our <strong>Blog</strong></h2>
-                <div class="line">
-                    <hr>
-                </div>
-                <div class="clearfix"></div>
+<div class="text">
+<div class="section-title" style="min-height: 650px" >
+                <h2><strong>Blog</strong></h2>
+                
                 <!--<p class="intro"><?php echo $page->post_content; ?></p> -->
                 <?php 
                 	$posts = get_posts();
-
                 	foreach ($posts as $obj)
                 	{
                 		$post = (array)$obj;
                 		?>
-                		<div class="postBox">
-	                		<h2><?php echo $post['post_title']?></h2>
+
+                		<section class="article" id="<?php echo $post['ID'] ?>">
+                			<div class="postBox">
+	                		<h4><a href="/?p=<?php echo $post['ID'] ?>" class="alink"><?php echo $post['post_title']?></a></h4>
+	                		<div class="line">
+                    			<hr>
+                			</div>
+                			<div class="clearfix"></div>
 	                		<p class="intro"><?php echo $post['post_content'] ?></p>
+	                		<?php  $user = get_userdata($post['post_author']);
+	                			$cats = array();
+								foreach(wp_get_post_categories($post['ID']) as $c)
+								{
+									$cat = get_category($c);
+									array_push($cats,$cat->name);
+								}
+
+								if(sizeOf($cats)>0)
+								{
+									$post_categories = implode(',',$cats);
+								}
+								else
+								{
+									$post_categories = "Not Assigned";
+								}
+	                		?>
 	                		<div class="infoBox">
-	                			Created: <?php echo $post['post_date'] ?>
+	                			Categories: <?php echo $post_categories; ?>
+	                		</div>
+	                		<div class="clearfix"></div>
+	                		<div class="infoBox">
+	                			Created By: <?php echo $user->user_login." on ".$post['post_date'] ?>
+
 	                		</div>
                 		</div>
-                		<div class="line">
-                    	<hr>
-                		</div>
-                		<div class="clearfix"></div>
+                		
+                		</section>
+                		
                 		<?php
                 	}
                 	?>
