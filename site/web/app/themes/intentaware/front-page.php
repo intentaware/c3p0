c247fa13e360c39579dd5a53ca93229f6e17f545
Rehman <manichilll@yahoo.com>
2015-12-11 18:44:24 +0500

    Child loop working with all the template loading function.

--- site/web/app/themes/intentaware/front-page.php
+++ site/web/app/themes/intentaware/front-page.php
@@ -6,6 +6,9 @@ if(has_post_thumbnail(get_the_ID()))
 {
 	$thumbId = get_post_thumbnail_id();
 	$thumbUrl = wp_get_attachment_url($thumbId);
+	$my_wp_query = new WP_Query();
+	$all_wp_pages = $my_wp_query->query(array('post_type' => 'page'));
+	$children = get_page_children( get_the_ID(), $all_wp_pages );
 	?>
 	<div id="home" class="text-center" style="background-image: url(<?php echo $thumbUrl; ?>)">
     	<div class="overlay">
@@ -31,12 +34,15 @@ else
 	<?php
 }
 ?>
-  
-
+<?php 	$mypages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'post_date', 'sort_order' => 'asce' ) );
+foreach( $mypages as $page )
+{
+	$content = $page->post_content;
+ 	if ( ! $content )
+ 	$content = apply_filters( 'the_content', $content );
+ 	$childTemplate = get_page_template_slug($page->ID);
+ 	include(locate_template($childTemplate));
+}
+?>
 <?php endwhile; ?>
-<?php get_template_part('templates/about'); ?>
-<?php get_template_part('templates/plateforms'); ?>
-<?php get_template_part('templates/labs'); ?>
-<?php get_template_part('templates/brandGuideline'); ?>
-<?php get_template_part('templates/opt-out'); ?>
-<?php get_template_part('templates/contact'); ?>
+
