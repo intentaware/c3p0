48f219b5c9e30833df305114a14f7bf7ce4b8ad1
Rehman <manichilll@yahoo.com>
2015-12-11 17:08:17 +0500

    Featured Image dynamic support included

--- site/web/app/themes/intentaware/front-page.php
+++ site/web/app/themes/intentaware/front-page.php
@@ -1,17 +1,38 @@
 
  <!--Template Name: Front Page -->
 
-<?php while (have_posts()) : the_post(); ?>
+<?php while (have_posts()) : the_post();
+if(has_post_thumbnail(get_the_ID()))
+{
+	$thumbId = get_post_thumbnail_id();
+	$thumbUrl = wp_get_attachment_url($thumbId);
+	?>
+	<div id="home" class="text-center" style="background-image: url(<?php echo $thumbUrl; ?>)">
+    	<div class="overlay">
+        	<div class="content">
+            	<h1>Welcome on <strong><span class="color"><?php the_title() ?></span></strong></h1>
+            	<p class="lead"><?php the_content() ?></p>
+            	<a><span id="#tf-about" class="glyphicon glyphicon-menu-down arrow-down" ></span></a>
+        	</div>
+    	</div>
+	</div>
+	<?php
+}
+else
+{
+	?>
+	<div id="home" class="text-center" style="color: #5a5a5a">
+        	<div class="content">
+            	<h1>Welcome on <strong><span class="color"><?php the_title() ?></span></strong></h1>
+            	<p class="intro"><?php the_content() ?></p>
+            	<a><span id="#tf-about" class="glyphicon glyphicon-menu-down arrow-down" ></span></a>
+        	</div>
+	</div>
+	<?php
+}
+?>
   
-  <div id="tf-home" class="text-center">
-        <div class="overlay">
-            <div class="content">
-                <h1>Welcome on <strong><span class="color"><?php the_title() ?></span></strong></h1>
-                <p class="lead"><?php the_content() ?></p>
-                <a><span id="#tf-about" class="glyphicon glyphicon-menu-down arrow-down" ></span></a>
-            </div>
-        </div>
-    </div>
+
 <?php endwhile; ?>
 <?php get_template_part('templates/about'); ?>
 <?php get_template_part('templates/plateforms'); ?>
