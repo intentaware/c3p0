ce8dc30f5352a465e4b24ff826f73922b94276f2
Rehman <manichilll@yahoo.com>
2015-12-16 18:16:24 +0500

    Style modification and login link added

--- site/web/app/themes/intentaware/templates/footer.php
+++ site/web/app/themes/intentaware/templates/footer.php
@@ -1,10 +1,15 @@
 </body>
-<div class="footer">
+<div class="mdl-mini-footer">
 	<nav id="footer">
 	    <div class="container">
 	        <div class="text-center">
-	            <p>ALL RIGHTS RESERVED. COPYRIGHT © 2015. Designed by <a href="#">IntentAware</a></p>
+	            ALL RIGHTS RESERVED. COPYRIGHT © 2015. Designed by <a href="#">IntentAware</a>
 	        </div>
 	    </div>
 	</nav>
+	<div class="">
+		<div class="container text-center">
+		<a href="<?php echo wp_login_url(); ?>">Login</a>
+	</div>
+	</div>
 </div>
