1cf54acc7d6922c7b65a508654d2d5cca8deca85
Rehman <manichilll@yahoo.com>
2015-12-24 20:40:44 +0500

    Corrections made

--- site/web/app/themes/intentaware/assets/scripts/custom_built.js
+++ site/web/app/themes/intentaware/assets/scripts/custom_built.js
@@ -1,22 +1,31 @@
 jQuery(document).ready(function($){
 	$("#navigation").find("ul.sub-menu").addClass("mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect").attr("for","sub-menu");
-console.log($('.rotate'));
-	$('.rotate').each(function(){
-		
+	
+	$('.rotate').each(function()
+	{
 		var el = $(this);
 		var text = $(this).html().split(",");
 		el.html(text[0]);
-		setInterval(function() {
-			el.animate({
+		setInterval(function()
+		{
+			el.animate(
+			{
               textShadowBlur:20,
               opacity: 0
-            }, 500 , function() {
-              index = $.inArray(el.html(), text)
-              if((index + 1) == text.length) index = -1
-              el.text(text[index + 1]).animate({
-                textShadowBlur:0,
-                opacity: 1
-              }, 500 );
+            },
+            500 , function() 
+            {
+              index = $.inArray(el.html(), text);
+              if((index + 1) === text.length) 
+              	{
+              		index = -1;
+              		el.text(text[index + 1]).animate(
+              		{
+                		textShadowBlur:0,
+                		opacity: 1
+              		}, 
+              		500 );
+              	}
             });
 		}, 2000);
 	});
