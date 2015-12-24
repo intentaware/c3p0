c0d2d59b0bffbcf33299c9ae14ed5a2805f1a6cd
Rehman <manichilll@yahoo.com>
2015-12-24 19:22:47 +0500

    Fixing

---
+++ release.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://sam.zoy.org/wtfpl/COPYING for more details.
+
+# for fgbg in 38 48 ; do #Foreground/Background
+#   for color in {0..256} ; do #Colors
+#     #Display the color
+#     printf "\e[${fgbg};5;${color}m ${color}\t\e[0m"
+#     #Display 10 colors per lines
+#     if [ $((($color + 1) % 10)) == 0 ] ; then
+#       printf "\n"
+#     fi
+#   done
+#   printf "\n\n"
+# done
+
+VERSION=$(date +%y.%m.%d)
+
+do_rsync () {
+  rsync -aP --delete --exclude ".git"  --exclude "node*" "site/" "release"
+}
+
+do_clone () {
+  git clone git@github.com:intentaware/c3p0.git --branch release --single-branch release
+}
+
+do_pull () {
+  cd "release"
+  git pull
+  cd ".."
+}
+
+do_push () {
+  git push origin
+}
+
+do_release_management () {
+  cd "release"
+  git add "." && git commit -m $VERSION
+  git tag -a $VERSION -m $VERSION
+  git push && git push --tags
+  cd ".."
+}
+
+
+if [ -d release/ ] ; then
+  do_pull
+  do_rsync
+  do_release_management
+else
+  do_clone
+  do_rsync
+  do_release_management
+fi
---
+++ release/web/app/themes/intentaware/templates/header.php
@@ -0,0 +1,19 @@
+<header class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+  <div class="android-header mdl-layout__header mdl-layout__header--waterfall">
+    <div class="mdl-layout__header-row">
+      <span class="android-title mdl-layout-title">
+          <a href="<?= esc_url(home_url('/')); ?>"><img class="brand android-logo-image" src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/logo-full.png"></a>
+      </span>
+      <div class="android-header-spacer mdl-layout-spacer"></div>
+      <div class="android-navigation-container">
+        <nav class="android-navigation mdl-navigation" id="navigation">
+          <?php
+            if (has_nav_menu('primary_navigation')) :
+              wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+            endif;
+          ?>
+        </nav>
+      </div>
+    </div>
+  </div>
+</header>
