9249f29bf31f8d75ccfb423394ff08b632f7cb63
Rehman <manichilll@yahoo.com>
2015-12-24 19:53:29 +0500

    15.12.24

--- web/app/themes/intentaware/assets/scripts/custom_built.js
+++ web/app/themes/intentaware/assets/scripts/custom_built.js
@@ -10,9 +10,10 @@ console.log($('.rotate'));
 			el.animate({
               textShadowBlur:20,
               opacity: 0
-            }, 500 , function() {
-              index = $.inArray(el.html(), text)
-              if((index + 1) == text.length) index = -1
+            }, 500 , function() 
+            {
+              index = $.inArray(el.html(), text);
+              if((index + 1) === text.length) index = -1;
               el.text(text[index + 1]).animate({
                 textShadowBlur:0,
                 opacity: 1
